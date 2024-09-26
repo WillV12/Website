293aa72ae2f298f310777b2a3ec7dd22f802b693
Will Vanderploeg <thatmurphyboi@gmail.com>
2024-09-26 13:50:24 -0400

    Minor additions

--- HTML/main.js
+++ HTML/main.js
@@ -1,1 +1,9 @@
+document.getElementById('colorInput').addEventListener('input', function() {
+    const color = this.value; 
+});
+
+document.getElementById('backgroundInput').addEventListener('input', function() {
+    const color = this.value;
+    
+});
 
